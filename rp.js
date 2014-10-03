5121694a37af36b50638aaf858cd9625c02018b4
Rodrigo Setti <rodrigosetti@gmail.com>
2014-10-03 11:48:59 -0700

    Tweak comments

--- rp.js
+++ rp.js
@@ -2,27 +2,23 @@
 
 var _ = require('lodash');
 
-/*
-# Reactive Programming in Javascript
-
-## Concepts
-
- * Signal: some value that varies in time (t -> s)
- * Signal function : transforms a signal into another
-
-*/
-
+/**
+ *  Reactive Programming in Javascript
+ *
+ * - Signal: some value that varies in time (t -> s)
+ * - Signal function : transforms a signal into another
+ */
 var rp = module.exports = {
-    // ### Sources
-
+    /**
+     * The constant signal function
+     */
     constant : _.constant,
 
-    time : function () {
-        return rp.compose(rp.constant(1), rp.integral());
-    },
-
-    // ### Combinators
-
+    /**
+     * Numerical integration of the signal value with integration constant c.
+     *
+     * Works with scalar and vector values.
+     */
     integral : function (c) {
         var accum = c || 0;
         return function(dt, value) {
@@ -37,6 +33,16 @@ var rp = module.exports = {
     },
 
     /**
+     * Outputs the current time.
+     */
+    time : function () {
+        return rp.compose(rp.constant(1), rp.integral());
+    },
+
+    /**
+     * The limit when times goes to infinity for the feedback of a signal
+     * function "sf", is the fixed point of "sf.
+     *
      *     initialValue --- sf ---+---> y
      *                      ^     |
      *                      |     |
@@ -79,17 +85,22 @@ var rp = module.exports = {
         };
     },
 
-    // ### Combinators to work with pure functions
-
+    /**
+     * Lift a pure function to a signal function
+     */
     lift : function (f) {
         return function (dt, value) {
             return f(value);
         };
     },
 
-    // ### Switches
-
     /**
+     * dSwitch (delayed switch)
+     *
+     * Change the underlying signal function once the predicate is true, the
+     * second signal function is constructed by the third parameter by the
+     * current value.
+     *
      *                      / +------ sf --+
      *       +------------+                |--> y
      *       |                +------ sf' -+
@@ -112,9 +123,12 @@ var rp = module.exports = {
         };
     },
 
-    // ### React
-
     /**
+     * React
+     * 
+     * Uses the Euler Method to perform numerical integrations of the Ordinary
+     * Differential Equation encoded as the first parameter signal function.
+     *
      * @param sf - signal function from *any* to true|false (continue or stop)
      * @param input (optional) - (function|array) of { dt: double, value: any }
      */
@@ -132,6 +146,5 @@ var rp = module.exports = {
             });
         }
     }
-
 };
 
